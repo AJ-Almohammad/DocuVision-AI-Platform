1f0a6d7bf4590bdde79c09023640db8c77665e7c
AJ-Almohammad <ajaber1973@web.de>
2025-10-04 18:18:52 +0200

    Fix port from 8001 to 8000 in startup script

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install -r requirements.txt
-gunicorn --bind=0.0.0.0:8001 --workers=4 --chdir ./src/api simple_main:app
+gunicorn --bind=0.0.0.0:8000 --workers=4 --chdir ./src/api simple_main:app
